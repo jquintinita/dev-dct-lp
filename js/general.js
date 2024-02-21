9e76e61dbbe0bf16de7e1c02e7423600ac89b4b0
Joseph Quintinita <joseph_quintinita@worldvision.ca>
2024-02-21 10:35:23 +0800

    updated homepage

--- js/general.js
+++ js/general.js
@@ -16,6 +16,46 @@ jQuery(function(){
       });
 
 
+
+      $('.promo-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
+
+
       //Mobile Nav
     $(".burger-btn").click(function(){
         if($(this).hasClass('active')){
